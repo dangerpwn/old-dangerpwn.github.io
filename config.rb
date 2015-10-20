8afb065dad839f65216abcd823e1404939979434
Danger Pwn <dangerpwn@gmail.com>
2015-10-20 19:48:55 +1000

    Added Casper theme

--- config.rb
+++ config.rb
@@ -10,8 +10,8 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
+  blog.sources = "articles/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tag/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
@@ -21,23 +21,51 @@ activate :blog do |blog|
   # blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  # blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
+  blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
 
-activate :deploy do |deploy|
-    deploy.method = :git
-    deploy.branch = 'master'
-    deploy.build_before = true
+set :casper, {
+  blog: {
+    url: 'http://www.example.com',
+    name: 'Middleman',
+    description: 'Makes developing websites simple.',
+    date_format: '%d %B %Y',
+    navigation: true,
+    logo: nil # Optional
+  },
+  author: {
+    name: 'Middleman',
+    bio: nil, # Optional
+    location: nil, # Optional
+    website: nil, # Optional
+    gravatar_email: nil, # Optional
+    twitter: nil # Optional
+  },
+  navigation: {
+    "Home" => "/"
+  }
+}
+
+page '/feed.xml', layout: false
+page '/sitemap.xml', layout: false
+
+ignore '/partials/*'
+
+ready do
+  blog.tags.each do |tag, articles|
+    proxy "/tag/#{tag.downcase.parameterize}/feed.xml", '/feed.xml', layout: false do
+      @tagname = tag
+      @articles = articles[0..5]
+    end
+  end
+
+  proxy "/author/#{blog_author.name.parameterize}.html", '/author.html', ignore: true
 end
-
-activate :directory_indexes
-
-page "/feed.xml", layout: false
 
 ###
 # Compass
@@ -77,7 +105,16 @@ page "/feed.xml", layout: false
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
+
+# Pretty URLs - http://middlemanapp.com/basics/pretty-urls/
+activate :directory_indexes
+
+# Middleman-Syntax - https://github.com/middleman/middleman-syntax
+set :haml, { ugly: true }
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true, footnotes: true, link_attributes: { rel: 'nofollow' }, tables: true
+activate :syntax, line_numbers: false
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -91,6 +128,8 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+set :partials_dir, 'partials'
 
 # Build-specific configuration
 configure :build do
